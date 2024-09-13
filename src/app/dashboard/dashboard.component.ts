fad164437a31408e2c40b0542a4a5670acb3c9c5
jonatanlopez22 <jonatanlopez696@gmail.com>
2024-09-12 21:56:52 -0500

    first commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [RouterModule, SidemenuComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export  default class DashboardComponent {
+
+}
